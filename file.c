6a38588d08fe9ad52c648d21f399b58955a5e0ce
Rakhee <rksharma19896@gmail.com>
2022-01-04 01:13:04 -0500

    Remove unused variables and R functionality

--- file.c
+++ file.c
@@ -7,15 +7,14 @@
 #include "fat32disk.h"
 #include "directory.h"
 #include "recover.h"
-#define print(d) printf("%d\n",d)
 
 int main(int argc, char **argv){
     int flag=0, anyOption=0;
-    int information=0, listRootDir=0, recoverRegFile=0, sha=0;
+    int information=0, listRootDir=0, recoverFiles=0, sha=0;
     char *recOptarg, *shaOptarg;
 
     // go through all of the flags
-    while((flag=getopt(argc, argv, "ilr:R:s:")) != -1){
+    while((flag=getopt(argc, argv, "ilr:s:")) != -1){
         anyOption=1;
         switch(flag){
             case 'i':
@@ -25,11 +24,8 @@ int main(int argc, char **argv){
                 listRootDir=1;
                 break;
             case 'r':
-                recoverRegFile=1;
+                recoverFiles=1;
                 recOptarg = optarg;
-                break;
-            case 'R':
-                printf("case R");
                 break;
             case 's':
                 sha = 1;
@@ -40,20 +36,20 @@ int main(int argc, char **argv){
                 break;
         }
     }
-    
+
     if (anyOption==0 || optind==argc)
         showUsage();
 
     // get file system information
     int fd = getFileDirectory(argv[optind]);
     BootEntry* disk = readFileSystem(fd);
-    
+
     if (information)
         showDiskInformation(disk);
     else if (listRootDir)
         getRootDirectoryEntries(fd, disk);
     else if(sha){ // use sha only when -r flag is given
-        if(recoverRegFile){
+        if(recoverFiles){
             if (recOptarg == NULL)
             showUsage();
         recoverFile(fd, disk, recOptarg, shaOptarg);
@@ -62,7 +58,7 @@ int main(int argc, char **argv){
             showUsage();
         }
     }
-    else if(recoverRegFile && !sha){ // only recover; no sha
+    else if(recoverFiles && !sha){ // only recover; no sha
         if (recOptarg == NULL)
             showUsage();
         recoverFile(fd, disk, recOptarg, NULL);
